979d253d4c44bfd63f74e87bdadde62470858b80
art poltavsky <artinte7@gmail.com>
2019-04-29 00:03:22 +0300

    major improvements

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -7,6 +7,10 @@ $(document).ready(function($) {
 
 	"use strict";
 
+  $('.your-class').slick({
+		setting-name: setting-value
+	});
+
 	$(window).stellar({
     responsive: false,
     parallaxBackgrounds: true,
